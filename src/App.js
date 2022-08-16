bce5c2f92e5c9f179ec8f8803ac1ea19bad2d0d1
BeTetiana <tibedina@gmail.com>
2022-08-16 17:56:50 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
+      <div className="Container">
         <Weather defaultCity="Kyiv" />
       </div>
     </div>
